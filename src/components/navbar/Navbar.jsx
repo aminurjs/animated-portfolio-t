35afe36e52e8a5435fd5245bce239106bf9289ef
Aminur Rahman <dev.aminur@gmail.com>
2023-12-30 22:43:18 +0600

    App setup

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,14 @@
+import "./navbar.scss";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      {/* Sidebar */}
+      <div className="wrapper">
+        <span>Aminur Rahman</span>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
