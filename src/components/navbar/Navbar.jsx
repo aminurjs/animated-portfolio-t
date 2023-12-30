7441a25e88b21c33134ed6a7a16a56dd5063eddb
Aminur Rahman <dev.aminur@gmail.com>
2023-12-30 23:24:25 +0600

    Navbar created

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -6,6 +6,14 @@ const Navbar = () => {
       {/* Sidebar */}
       <div className="wrapper">
         <span>Aminur Rahman</span>
+        <div className="social">
+          <a href="#">
+            <img src="./github.png" alt="" />
+            <img src="./linkedin.png" alt="" />
+            <img src="./facebook.png" alt="" />
+            <img src="./instagram.png" alt="" />
+          </a>
+        </div>
       </div>
     </div>
   );
